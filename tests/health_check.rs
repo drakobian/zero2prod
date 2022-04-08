ce4bf112153c42734b12f91f2d6dcafdd08cdd8a
drakobian <drake.bennion@chghealthcare.com>
2022-04-08 11:42:11 -0600

    test: query db for integration test expectations

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -23,7 +23,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
     let configuration = get_configuration().expect("failed to get configuration");
     let connection_string = configuration.database.connection_string();
-    let _connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to postgres");
     let client = reqwest::Client::new();
@@ -38,6 +38,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert!(response.status().is_success());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
