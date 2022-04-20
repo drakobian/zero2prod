fa04438aa41568294729b2b2bec21b7077c193ea
drakobian <drake.bennion@chghealthcare.com>
2022-04-20 15:11:21 -0600

    feat: dockerize and prepare for deploy

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,9 +1,16 @@
 use secrecy::{ExposeSecret, Secret};
+use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -40,8 +47,47 @@ impl DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
+    let base_path = std::env::current_dir().expect("failed to determine current directory");
+    let config_directory = base_path.join("configuration");
 
-    settings.merge(config::File::with_name("configuration"))?;
+    settings.merge(config::File::from(config_directory.join("base")).required(true))?;
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("failed to parse APP_ENVIRONMENT");
+
+    settings
+        .merge(config::File::from(config_directory.join(environment.as_str())).required(true))?;
 
     settings.try_into()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either 'local' or 'production'.",
+                other
+            )),
+        }
+    }
 }
